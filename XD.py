95fb413a6f47c81b7e01edfa9caa023a4347360a
NINJA-XD <125014922+X-NINJA-XD@users.noreply.github.com>
2023-03-22 11:56:45 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,1 +1,13 @@
-
+import os,time,platform,sys
+os.system("xdg-open https://facebook.com/groups/peaky009/")
+os.system('clear')
+print('[>] Checking Updates')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+ print("Found 64 bit")
+ time.sleep(0.5)
+ import Ai
+else:
+ print('\033[1;31mOnly For 64 bit Device')
+ sys.exit()
